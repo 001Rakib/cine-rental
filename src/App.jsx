c7c4dbc92a3dcc2052e22baf9c22d6aaf11f8da1
001Rakib <rakibrakibrakib354@gmail.com>
2025-05-20 20:26:13 +0600

    created header sidebar and movie list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import MovieList from "./cine/MovieList";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <>
-      <h1>Cine rental coming</h1>
-    </>
+    <div className="max-w-screen-xl mx-auto">
+      <Header />
+      <main>
+        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
+    </div>
   );
 }
 
